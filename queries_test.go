a0d2a88d79b7ad53a82d65e9c9616a588137cfae
Roberto Guerra <uris77@gmail.com>
2020-08-01 16:29:56 -0600

    Can query by updated at

--- queries_test.go
+++ queries_test.go
@@ -16,9 +16,35 @@ func TestGetNewArrivals(t *testing.T) {
 	}
 	defer db.Close()
 
-	_, err = GetNewArrivals("2020-08-01", db)
+	arrivals, err := GetArrivals("2020-08-01", CreatedAt, db)
 	if err != nil {
 		t.Fatalf("failure executing query: %v", err)
+	}
+
+	if len(arrivals) == 0 {
+		t.Error("want: non empty array, got: empty array")
+	}
+
+}
+
+func TestGetArrivals_Updated(t *testing.T) {
+	cnf, err := ReadConf("sample_cnf.yaml", "test")
+	if err != nil {
+		t.Fatalf("failure reading cnf file %v", err)
+	}
+	db, err := CreateConnection(cnf)
+	if err != nil {
+		t.Fatalf("failure creating connection %v", err)
+	}
+	defer db.Close()
+
+	arrivals, err := GetArrivals("2020-08-01", UpdateAt, db)
+	if err != nil {
+		t.Fatalf("failure executing query: %v", err)
+	}
+
+	if len(arrivals) == 0 {
+		t.Error("want: non empty array, got: empty array")
 	}
 
 }
